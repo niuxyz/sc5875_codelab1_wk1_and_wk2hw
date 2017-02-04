8411a8e771142983ac13f61ec187abf53bd33875
Unknown <sc5875@nyu.edu>
2017-02-04 17:30:17 -0500

    Codelab_game
    Player Control complete
    player Sprite

---
+++ CodeLab1-2017/Assets/scripts/PlayerControlScript.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControlScript : MonoBehaviour {
+
+	public float Maxspeed;
+	public string cntrlx;
+	public string cntrly;
+
+	//The Direction and Speed Information
+	private Vector2 velocity;
+
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Move();
+	}
+
+	void Move(){
+		Vector2 targetVelocity = new Vector2(Input.GetAxis(cntrlx), Input.GetAxis(cntrly))*Maxspeed;
+		velocity = Vector2.Lerp(velocity, targetVelocity, 0.1f);
+		GetComponent<Rigidbody2D>().velocity = velocity;
+
+		Color playerColor = new Color(1,1,1,velocity.magnitude/Maxspeed);
+		GetComponent<SpriteRenderer>().color =  playerColor;
+	}
+}
